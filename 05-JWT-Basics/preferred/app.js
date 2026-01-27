c9c6e5964a47908909c02e9e76629dbff7c142d1
Tenzin <tenzin.thakuri01@gmail.com>
2026-01-26 22:52:22 -0500

    Authentication with jwt token

---
+++ 05-JWT-Basics/preferred/app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const authRoutes = require("./routes/user");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use("/api/v1", authRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
